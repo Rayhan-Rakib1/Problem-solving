25b6929bdc422adde2b2f2e78129e0cb86541228
Rayhan-Rakib1 <rayhanrakib448@gmail.com>
2026-06-04 20:44:22 +0600

    day 2 added

--- src/04-06-2026.js
+++ src/04-06-2026.js
@@ -6,3 +6,10 @@ function capitalize (sentence){
 
 const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"
 console.log(capitalize(text));
+function capitalize (sentence){
+   return sentence.split('. ').map(sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1)).join('.')
+
+}
+
+const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"
+console.log(capitalize(text));

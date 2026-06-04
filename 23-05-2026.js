f88c7631c7e8e94fbd2f8746915d8d85f4f4ef5d
Rayhan-Rakib1 <rayhanrakib448@gmail.com>
2026-06-04 20:29:15 +0600

    javascript problem day 1

--- 23-05-2026.js
+++ 23-05-2026.js
@@ -1,7 +1,68 @@
-// let abc = "rakib";
-// abc 2 = "r";
-// console.log(abc);
+// // Reverse a string
+// const a = "I am rayhan rakib . I am a full stack developer . I am good as backend technology"
+// const reversed = a.split("").reverse().join('');
+// console.log(reversed)
 
-let a = 100;
-let b = a++;
-console.log(a + b );
+// Find the largest number in an array
+// const numbers = [3, 5, 7, 2, 8, 10];
+
+// let largest =  numbers[0];
+// for(let i = 1 ; i < numbers.length; i++){
+//     if(numbers[i] > largest){
+//         largest = numbers[i]
+//     }
+// }
+// console.log(largest);
+
+// Find the smallest number in an array
+// const numbers = [3, 5, 7, 2, 8, 10];
+
+// let smallest =  numbers[0];
+// for(let i = 1 ; i < numbers.length; i++){
+//     if(numbers[i] < smallest){
+//         smallest = numbers[i]
+//     }
+// }
+// console.log(smallest);
+
+
+
+// Count vowels in a string
+// const str = "I am rayhan rakib . I am a full stack developer . I am good as backend technology"
+// const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+// let count = 0;
+
+// for (let char of str){
+//     if(vowels.includes(char)){
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+
+// Count consonants in a string
+// const str = "I am rayhan rakib . I am a full stack developer . I am good as backend technology"
+// const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+// let count = 0;
+
+// for (let char of str){
+//     if(!vowels.includes(char) && char.match(/[a-zA-Z]/)){
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+
+// Capitalize the first letter of a string
+// function capitalize (sentence){
+//    return sentence.split('. ').map(sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1)).join('.')
+
+// }
+
+// const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"
+// console.log(capitalize(text));
+
+
+// const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"

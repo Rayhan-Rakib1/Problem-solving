2460b9024ef9bcd8217909afb6d45c5b1094f2ea
Rayhan-Rakib1 <rayhanrakib448@gmail.com>
2026-06-04 20:31:51 +0600

    day 1

--- 23-05-2026.js
+++ 23-05-2026.js
@@ -66,3 +66,11 @@
 
 
 const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"
+// Capitalize the first letter of a string
+function capitalize (sentence){
+   return sentence.split('. ').map(sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1)).join('.')
+
+}
+
+const text = "i am rayhan rakib . i am a full stack developer . i am good as backend technology"
+console.log(capitalize(text));
